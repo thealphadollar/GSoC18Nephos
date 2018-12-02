22a9730f212e5b9cc5feaee0b734a70cc58daf73
matejmecka <matej.plavevski+github@gmail.com>
2018-12-02 23:21:28 +0100

    some docs

--- nephos/web/tests/base.py
+++ nephos/web/tests/base.py
@@ -5,9 +5,18 @@ from webServer import APP
 
 
 class BaseTestCase(TestCase):
+    """
+    Reusable Template for a Test Case
+    """
     def create_app(self):
+        """
+        Create the App so you can test it
+        """
         # executed prior to each test
         def setUp(self):
+            """
+            Set up for the Test Case
+            """
             APP.config['TESTING'] = True
             APP.config['WTF_CSRF_ENABLED'] = False
             APP.config['DEBUG'] = False
@@ -17,10 +26,13 @@ class BaseTestCase(TestCase):
             DB.create_all()
 
         return APP
-        
+
         # executed after each test
         def tearDown(self):
-            db.drop_all()
+            """
+            When finished to teardown
+            """
+            DB.drop_all()
 
 
 if __name__ == "__main__":
