7ea0d4f1720eacf103d1c9a9b33a51987eeb1cdf
matejmecka <matej.plavevski+github@gmail.com>
2018-12-10 07:37:16 +0100

    no use for a file

--- nephos/web/info_panel/static/scripts.js
+++
@@ -1,9 +0,0 @@
-function askConfirmation(url){
-    let confirmation = ('This will delete the following channel! Are you sure you want to do this?')
-    if(confirmation === true){
-        fetch()
-    }
-    else{
-
-    }
-}
